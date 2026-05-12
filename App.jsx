6abd904e29f35af41ecf9e32c8d6f1cadcd37fc6
Hshefat <shefat.hasnain@gmail.com>
2026-05-12 09:47:09 +0600

    homeCompnent

--- App.jsx
+++ App.jsx
@@ -1,19 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-  Text,
-} from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import HomeCompo from './src/components/homeCompo';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -21,24 +10,9 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello Shefat</Text>
-      </View>
+      <HomeCompo />
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center', // vertical center
-    alignItems: 'center', // horizontal center
-  },
-  text: {
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-});
 
 export default App;
